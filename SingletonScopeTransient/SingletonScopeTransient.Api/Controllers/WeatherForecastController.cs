eead958c5ad5af812a61bab56a45fadd8b1527e6
marciomonzon <marcio.pcmonzon@gmail.com>
2022-11-11 21:54:10 -0300

    final commit

--- SingletonScopeTransient/SingletonScopeTransient.Api/Controllers/WeatherForecastController.cs
+++ SingletonScopeTransient/SingletonScopeTransient.Api/Controllers/WeatherForecastController.cs
@@ -9,9 +9,13 @@ public class WeatherForecastController : ControllerBase
     private readonly ISingleton _singleton;
     private readonly IScoped _scoped;
     private readonly ITransient _transient;
+    private readonly IScoped _secondScoped;
+    private readonly ITransient _secondTransient;
 
-    public WeatherForecastController(ISingleton singleton, IScoped scoped, ITransient transient)
+    public WeatherForecastController(ISingleton singleton, IScoped scoped, ITransient transient, IScoped secondScoped, ITransient secondTransient)
     {
+        _secondTransient = secondTransient;
+        _secondScoped = secondScoped;
         _transient = transient;
         _scoped = scoped;
         _singleton = singleton;
@@ -28,7 +32,10 @@ public class WeatherForecastController : ControllerBase
         {
             SingletonValue = singletonGuid,
             ScopedValue = scopedGuid,
-            TransientValue = transientGuid
+            TransientValue = transientGuid,
+
+            SecondScopedValue = _secondScoped.Value,
+            SecondTransientValue = _secondTransient.Value
         };
 
         return Ok(response);
@@ -37,7 +44,10 @@ public class WeatherForecastController : ControllerBase
 
 public class ResponseModel
 {
-    public Guid SingletonValue {get; set;}
-    public Guid ScopedValue {get; set;}
-    public Guid TransientValue {get; set;}
+    public Guid SingletonValue { get; set; }
+    public Guid ScopedValue { get; set; }
+    public Guid TransientValue { get; set; }
+
+    public Guid SecondScopedValue { get; set; }
+    public Guid SecondTransientValue { get; set; }
 }
